252f32eb5a9b3682ea695977a9a73dedf93225bd
AasathGoSetup <125187263+AasathAk@users.noreply.github.com>
2025-05-12 11:26:37 +0530

    update use asset Function

--- resources/views/include/basicjs.blade.php
+++ resources/views/include/basicjs.blade.php
@@ -1,17 +1,15 @@
-    
     @include('include.loginpopup')
     
-
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    <script src="/front_asset/js/jquery.min.js" type="text/javascript"></script>
-    <script src="/front_asset/js/modernizr.js" type="text/javascript"></script>
-    <script src="/front_asset/js/script.js" type="text/javascript"></script>
-    <script src="/front_asset/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="/front_asset/js/wow.min.js" type="text/javascript"></script>
-    <script src="/front_asset/js/slick.min.js" type="text/javascript"></script>
-    <script src="/front_asset/js/parallax.js" type="text/javascript"></script>
-    <script src="/front_asset/js/select-chosen.js" type="text/javascript"></script>
+    <script src="{{ asset('front_asset/js/jquery.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('front_asset/js/modernizr.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('front_asset/js/script.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('front_asset/js/bootstrap.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('front_asset/js/wow.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('front_asset/js/slick.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('front_asset/js/parallax.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('front_asset/js/select-chosen.js') }}" type="text/javascript"></script>
 
     {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js" integrity="sha512-Tn2m0TIpgVyTzzvmxLNuqbSJH3JP8jm+Cy3hvHrW7ndTDcJ1w5mBiksqDBb8GpE2ksktFvDB/ykZ0mDpsZj20w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/brands.min.js" integrity="sha512-8Jp5PI7qKEn304XONokQQRgiu/1P9kTBlvpLc7zRukkTBYGKt6z4CkwJUJhCwwWYnjSdxJcGqW9ifT7ZxPNgbg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -45,7 +43,4 @@
         function updateResume() {
           document.getElementById("updateResume").style.display = "none";
         }
-
-      
-
     </script>
